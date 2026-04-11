16a4b473b4127a7f622cd492d1b33447b52df554
Pablo Perez <pabloperez@visualandgrowth.es>
2026-04-12 01:21:44 +0200

    fix: NotificationBell Uint8Array type error

--- components/NotificationBell.tsx
+++ components/NotificationBell.tsx
@@ -67,7 +67,7 @@ async function registerPush() {
   }
 }
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): Uint8Array<ArrayBuffer> {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = atob(base64)
